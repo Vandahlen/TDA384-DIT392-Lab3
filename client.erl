32e954377d4ffd1d7d37141aed2ae00374846cd2
Jakob <jakobah@chalmer.se>
2026-03-09 13:28:46 +0100

    Final changes and comments added

--- client.erl
+++ client.erl
@@ -1,80 +1,99 @@
 -module(client).
 -export([handle/2, initial_state/3]).
 
-% This record defines the structure of the state of a client.
-% Add whatever other fields you need.
 -record(client_st, {
-    gui, % atom of the GUI process
-    nick, % nick/username of the client
-    server % atom of the chat server
+    gui,
+    nick,
+    server,
+    channels = #{} % Maps ChannelName -> ChannelPid for direct communication.
 }).
 
-% Return an initial state record. This is called from GUI.
-% Do not change the signature of this function.
 initial_state(Nick, GUIAtom, ServerAtom) ->
     #client_st{
         gui = GUIAtom,
         nick = Nick,
-        server = ServerAtom
+        server = ServerAtom,
+        channels = #{}
     }.
 
 % Join channel
 handle(St, {join, Channel}) ->
     Server = St#client_st.server,
     Nick = St#client_st.nick,
+    
+    % Catch crashes to prevent client failure and notify GUI.
     Result = case catch genserver:request(Server, {join, Channel, self(), Nick}) of
         {'EXIT', _} -> {error, server_not_reached, "Server timed out"};
         timeout_error -> {error, server_not_reached, "Server timed out"};
-        Other -> Other
+        Other -> Other 
     end,
-    {reply, Result, St};
+    
+    case Result of
+        {ok, ChannelPid} ->
+            % Store ChannelPid for direct messaging.
+            NewChannels = maps:put(Channel, ChannelPid, St#client_st.channels),
+            {reply, ok, St#client_st{channels = NewChannels}};
+        Error ->
+            {reply, Error, St}
+    end;
 
 % Leave channel
 handle(St, {leave, Channel}) ->
-    Server = St#client_st.server,
-    Result = case catch genserver:request(Server, {leave, Channel, self()}) of
-    {'EXIT', _} -> ok;
-    timeout_error -> ok;
-    Other -> Other
-    end,
-    {reply, Result, St};
+    ChannelPid = maps:get(Channel, St#client_st.channels, undefined),
+    case ChannelPid of
+        undefined -> 
+            {reply, {error, user_not_joined, "You are not in this channel"}, St};
+        _ ->
+            % Leave the channel process directly.
+            catch genserver:request(ChannelPid, {leave, self()}),
+            NewChannels = maps:remove(Channel, St#client_st.channels),
+            {reply, ok, St#client_st{channels = NewChannels}}
+    end;
 
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    Server = St#client_st.server,
-    Nick = St#client_st.nick,
+    Nick = St#client_st.nick, 
+    ChannelPid = maps:get(Channel, St#client_st.channels, undefined),
+    
+    case ChannelPid of
+        undefined -> 
+            % Fallback: Check with main server if channel doesn't exist locally.
+            Server = St#client_st.server,
+            TryResult = catch genserver:request(Server, {message_send, Channel, self(), Nick, Msg}), 
+            Result = case TryResult of
+                {'EXIT', _} -> {error, server_not_reached, "Server timed out"};
+                timeout_error -> {error, server_not_reached, "Server timed out"};
+                {error, user_not_joined, "Channel does not exist"} -> 
+                    {error, server_not_reached, "Channel does not exist"};
+                Other -> Other 
+            end,
+            {reply, Result, St};
+        _ ->
+            % Concurrency: Send directly to the channel process. Bypasses main server.
+            TryResult = catch genserver:request(ChannelPid, {message_send, self(), Nick, Msg}), 
+            Result = case TryResult of
+                {'EXIT', _} -> {error, server_not_reached, "Server timed out"};
+                timeout_error -> {error, server_not_reached, "Server timed out"};
+                Other -> Other 
+            end,
+            {reply, Result, St}
+    end;
 
-    TryResult = catch genserver:request(Server, {message_send, Channel, self(), Nick, Msg}),
-
-    Result = case TryResult of
-        {'EXIT', _} -> {error, server_not_reached, "Server timed out"};
-        timeout_error -> {error, server_not_reached, "Server timed out"};
-        {error, user_not_joined, "Channel does not exist"} -> 
-        {error, server_not_reached, "Channel does not exist"};
-        Other -> Other
-    end,
-    {reply, Result, St};
-
-% This case is only relevant for the distinction assignment!
-% Change nick (no check, local only)
+% Change nick
 handle(St, {nick, NewNick}) ->
     Server = St#client_st.server,
     OldNick = St#client_st.nick,
 
-    Result = genserver:request(Server, {nick, OldNick, NewNick, self()}),
+    % Catch server timeouts gracefully.
+    TryResult = catch genserver:request(Server, {nick, OldNick, NewNick, self()}),
     
-    case Result of
-        ok -> 
-            {reply, ok, St#client_st{nick = NewNick}};
-        {error, nick_taken, _} ->
-            {reply, {error, nick_taken, "Nick taken"}, St};
-        OtherError ->
-             {reply, OtherError, St}
+    case TryResult of
+        {'EXIT', _} -> {reply, {error, server_not_reached, "Server timed out"}, St};
+        timeout_error -> {reply, {error, server_not_reached, "Server timed out"}, St};
+        ok -> {reply, ok, St#client_st{nick = NewNick}}; 
+        {error, nick_taken, _} -> {reply, {error, nick_taken, "Nick taken"}, St}; 
+        OtherError -> {reply, OtherError, St}
     end;
-
-% ---------------------------------------------------------------------------
-% The cases below do not need to be changed...
-% But you should understand how they work!
 
 % Get current nick
 handle(St, whoami) ->
@@ -82,15 +101,14 @@ handle(St, whoami) ->
 
 % Incoming message (from channel, to GUI)
 handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
+    % Forward channel broadcast to GUI.
     gen_server:call(GUI, {message_receive, Channel, Nick++"> "++Msg}),
     {reply, ok, St} ;
 
-% Quit client via GUI
+% Quit client
 handle(St, quit) ->
-    % Any cleanup should happen here, but this is optional
     {reply, ok, St} ;
 
-% Catch-all for any unhandled requests
+% Catch-all
 handle(St, _Data) ->
     {reply, {error, not_implemented, "Client does not handle this command"}, St} .
-    
--- server.erl
+++ server.erl
@@ -1,101 +1,126 @@
 -module(server).
--export([start/1,stop/1]).
+-export([start/1, stop/1, channel_loop/2]). 
 
 -record(serverState, {
-    channels = #{},
-    nicks = #{}
+    channels = #{}, % Maps ChannelName -> ChannelPid
+    nicks = #{}     % Maps Nick -> Pid for distinction assignment
 }).
 
-% Start a new server process with the given name
-% Do not change the signature of this function.
+% Start and register the main server.
 start(ServerAtom) ->
-  genserver:start(ServerAtom, #serverState{channels = #{},nicks = #{} }, fun loop/2).
+    genserver:start(ServerAtom, #serverState{channels = #{}, nicks = #{}}, fun loop/2).
 
-% Stop the server process registered to the given name,
-% together with any other associated processes
+% Stop the server gracefully.
 stop(ServerAtom) ->
-   genserver:stop(ServerAtom).
+    % Halt child channels first to prevent orphaned processes.
+    catch genserver:request(ServerAtom, halt_associated_processes),
+    genserver:stop(ServerAtom).
 
-
+% Main server loop.
 loop(State, Request) ->
     case Request of
+        {join, Channel, Pid, Nick} ->
+            Channels = State#serverState.channels,
+            Nicks = State#serverState.nicks,
 
-    {join, Channel, Pid, Nick} ->
+            % Concurrency: Get existing channel process or spawn a new one.
+            {ChannelPid, NewChannels} = case maps:find(Channel, Channels) of
+                error ->
+                    CPid = spawn(server, channel_loop, [Channel, []]),
+                    {CPid, maps:put(Channel, CPid, Channels)};
+                {ok, CPid} -> 
+                    {CPid, Channels}
+            end,
 
-        Channels = State#serverState.channels,
-        Nicks = State#serverState.nicks,
+            % Forward join request to the channel process.
+            Ref = make_ref(),
+            ChannelPid ! {request, self(), Ref, {join, Pid}},
+            receive
+                {result, Ref, ok} ->
+                    NewNicks = maps:put(Nick, Pid, Nicks),
+                    % Return ChannelPid so the client can communicate directly.
+                    {reply, {ok, ChannelPid}, State#serverState{channels = NewChannels, nicks = NewNicks}};
+                {result, Ref, Error} ->
+                    {reply, Error, State#serverState{channels = NewChannels}}
+            end;
 
-        CurrentUsers = maps:get(Channel, Channels, []),
-        case lists:member(Pid, CurrentUsers) of
-            true ->
-                {reply, {error, user_already_joined, "You are already in this channel"}, State};
-            false ->
-                NewUsers = [Pid | CurrentUsers],
-                NewChannels = maps:put(Channel, NewUsers, Channels),
-                NewNicks = maps:put(Nick, Pid, Nicks),
-                {reply, ok, State#serverState{channels = NewChannels, nicks = NewNicks}}
-        end;
-
-    {leave, Channel, Pid} ->
-
-        Channels = State#serverState.channels,
-
-        case maps:is_key (Channel, Channels) of
-            false ->
-                {reply, {error, user_not_joined, "..."}, State};
-            true ->
-                CurrentUsers = maps:get(Channel, Channels),
-                case lists:member(Pid, CurrentUsers) of
-                    false ->
-                        {reply, {error, user_not_joined, "..."}, State};
-                    true ->
-                        NewUsers = lists:delete(Pid, CurrentUsers),
-                        NewChannels = maps:put(Channel, NewUsers, Channels),
-                        {reply, ok, State#serverState{channels = NewChannels}}
-                end
-        end;
-    
-    {nick, OldNick, NewNick, Pid} ->
-        Nicks = State#serverState.nicks,
+        {leave, Channel, Pid} ->
+            Channels = State#serverState.channels,
+            
+            case maps:find(Channel, Channels) of
+                error ->
+                    {reply, {error, user_not_joined, "Channel does not exist"}, State};
+                {ok, ChannelPid} ->
+                    % Forward leave request to the channel process.
+                    Ref = make_ref(),
+                    ChannelPid ! {request, self(), Ref, {leave, Pid}},
+                    receive
+                        {result, Ref, Reply} -> {reply, Reply, State}
+                    end
+            end;
         
-        case maps:is_key(NewNick, Nicks) of
-            true ->
-                {reply, {error, nick_taken, "Nick already taken"}, State};
-            false ->
-                NicksCleaned = maps:remove(OldNick, Nicks),
-                
-                NewNicks = maps:put(NewNick, Pid, NicksCleaned),
+        {nick, OldNick, NewNick, Pid} ->
+            Nicks = State#serverState.nicks,
+            
+            % Ensure nickname uniqueness.
+            case maps:is_key(NewNick, Nicks) of
+                true ->
+                    {reply, {error, nick_taken, "Nick already taken"}, State};
+                false ->
+                    NicksCleaned = maps:remove(OldNick, Nicks),
+                    NewNicks = maps:put(NewNick, Pid, NicksCleaned),
+                    {reply, ok, State#serverState{nicks = NewNicks}}
+            end;
 
-                {reply, ok, State#serverState{nicks = NewNicks}}
-        end;
+        {message_send, TargetChannel, _Pid, _Nick, _Msg} ->
+            % Fallback: Handle send requests from clients not joined locally.
+            Channels = State#serverState.channels,
+            case maps:find(TargetChannel, Channels) of
+                error -> {reply, {error, user_not_joined, "Channel does not exist"}, State};
+                {ok, _ChannelPid} -> {reply, {error, user_not_joined, "You are not in this channel"}, State}
+            end;
 
-  {message_send, TargetChannel, Pid, Nick, Msg} ->
-
-        Channels = State#serverState.channels,
-        
-        case maps:find(TargetChannel, Channels) of
-            error ->
-                {reply, {error, user_not_joined, "Channel does not exist"}, State};
-            {ok, Users} ->
-                case lists:member(Pid, Users) of
-                    false ->
-                        {reply, {error, user_not_joined, "You are not in this channel"}, State};
-                    true ->
-                        ServerPid = self(),
-                        spawn(fun() ->
-                            Recipients = lists:delete(Pid, Users),
-                            Sendmsg = fun(ReceiverPid) ->
-                                ReceiverPid ! {request, ServerPid, make_ref(), {message_receive, TargetChannel, Nick, Msg}}
-                            end,
-                            lists:foreach(Sendmsg, Recipients)
-                        end),
-                        
-                        {reply, ok, State}
-                end
-        end;
-    stop ->
-        {reply, ok, State};
-    
-    _ ->
+        halt_associated_processes -> 
+            % Terminate all child channels for a clean shutdown.
+            maps:foreach(fun(_Ch, CPid) -> exit(CPid, kill) end, State#serverState.channels),
+            {reply, ok, State};
+            
+        _ -> 
             {reply, {error, unknown_command, "Server received unknown command"}, State}
-end.
+    end.
+
+% Independent channel process. Survives main server crashes.
+channel_loop(Channel, Users) ->
+    receive
+        {request, From, Ref, {join, Pid}} ->
+            % Prevent duplicate joins.
+            case lists:member(Pid, Users) of
+                true ->
+                    From ! {result, Ref, {error, user_already_joined, "You are already in this channel"}},
+                    channel_loop(Channel, Users);
+                false ->
+                    From ! {result, Ref, ok},
+                    channel_loop(Channel, [Pid | Users])
+            end;
+            
+        {request, From, Ref, {leave, Pid}} ->
+            % Verify membership before removal.
+            case lists:member(Pid, Users) of
+                false ->
+                    From ! {result, Ref, {error, user_not_joined, "You are not in this channel"}},
+                    channel_loop(Channel, Users);
+                true ->
+                    From ! {result, Ref, ok},
+                    channel_loop(Channel, lists:delete(Pid, Users))
+            end;
+            
+        {request, From, Ref, {message_send, SenderPid, Nick, Msg}} ->
+            % Exclude sender to prevent message echo.
+            Recipients = lists:delete(SenderPid, Users),
+            lists:foreach(fun(UserPid) ->
+                % Broadcast message to all members.
+                UserPid ! {request, self(), make_ref(), {message_receive, Channel, Nick, Msg}}
+            end, Recipients),
+            From ! {result, Ref, ok},
+            channel_loop(Channel, Users)
+    end.
